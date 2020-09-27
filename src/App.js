781051a61af5124f237923738df15122555628f9
Talha Munir <46424469+mtalham@users.noreply.github.com>
2020-09-27 17:28:40 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './App.css';
-import FieldArrayForm from "./components/fieldArrayForm";
-import Testform from "./components/testform";
 import Form from "./components/Form";
-import {Input} from "./components/FormInputs";
+import {Checkbox, Input, SelectInput} from "./components/FormInputs";
 import SubmitButton from "./components/SubmitButton";
 
 function App() {
   const onSubmit = data => alert(`data: ${JSON.stringify(data)}`);
   return (
     <div className="App">
-      {/*<FieldArrayForm/>*/}
-      <Testform/>
       <Form onSubmit={onSubmit}>
         <Input label="Name" name="name" validation={{required: "Name is required"}}/>
         <Input label="Age" name="age" type="number" validation={{min: {value: 21, message: 'Min 21 is required'}}}/>
         <Input label="Email" name="email" type="email"/>
+        <Checkbox name="terms" label="Accept terms of agreement"/>
+        <SelectInput name="Flavours" options={Options} />
         <SubmitButton/>
       </Form>
     </div>
@@ -23,3 +21,9 @@ function App() {
 }
 
 export default App;
+
+const Options = [
+  {value: "chocolate", label: "Chocolate"},
+  {value: "vanilla", label: "Vanilla"},
+  {value: "strawberry", label: "Strawberry"},
+];
--- src/components/FormInputs.js
+++ src/components/FormInputs.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useFormContext} from "react-hook-form";
-import {Error} from "./inputs";
+import Select from "react-select";
 
 export const Input = ({label, name, required, validation, ...props}) => {
   const {register, errors} = useFormContext()
@@ -19,3 +19,44 @@ export const Input = ({label, name, required, validation, ...props}) => {
     </div>
   )
 }
+
+export const Checkbox = ({label, name}) => {
+  const {register} = useFormContext()
+
+  return (
+    <label>
+      <input style={{width: "14px"}} type="checkbox" name={name} ref={register}/>
+      {label}
+    </label>
+  )
+}
+
+export const SelectInput = ({name, options, isMulti = true}) => {
+  const [values, setSelectedValue] = useState({selectedOption: []});
+  const {register, setValue} = useFormContext()
+
+  useEffect(() => {
+    register({name: name});
+  }, [register, name]);
+
+  const handleMultiChange = selectedOption => {
+    setValue(name, selectedOption);
+    setSelectedValue({selectedOption})
+  };
+
+  return (
+    <>
+      <label>{name}</label>
+      <Select
+        name={name}
+        placeholder={name}
+        options={options}
+        onChange={handleMultiChange}
+        value={values.selectedOption}
+        isMulti={isMulti}
+      />
+    </>
+  );
+};
+
+export const Error = ({children}) => (<p style={{color: "orangered"}}>{children}</p>);
